1bef2cb9a82aa788d47ba4d83082f84291273d6f
GunelNovruzova <gunelvn@code.edu.az>
2021-12-13 23:30:54 -0800

    first library

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+        public void Minus(int a, int b)
+        {
+            Console.WriteLine(a-b);
+        }
+        public void Divide(int a, int b)
+        {
+            if (b!=0)
+            {
+                Console.WriteLine(a/b);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal1 = new Calculator();
+            cal1.Sum(5, 6);
+            cal1.Multiply(2, 10);
+            cal1.Divide(90, 5);
+            cal1.Minus(19, 9);
+        }
+    }
+}
